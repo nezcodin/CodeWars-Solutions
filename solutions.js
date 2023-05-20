d47973c0fd83b68fcb5d0525e5fdaa0d9af20331
nezcodin <ch.fusco104@gmail.com>
2023-05-20 00:40:29 -0500

    JS problem #3

--- solutions.js
+++ solutions.js
@@ -25,6 +25,7 @@ function digitize(n) {
 }
 
 // 2) Remove First and Last Character
+//  It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
 //pop first and last character
 //str.pop(0) AND str.pop(-1)
@@ -36,3 +37,25 @@ function removeChar(str) {
   const updatedStr = str.slice(0, 0) + str.slice(1, (str.length - 1))
   return updatedStr
 };
+
+// 3) Calculate average
+//  Write a function which calculates the average of the numbers in a given list. Empty arrays should return 0.
+
+// Average = all numbers in array added together divided by amount of numbers in array
+// use an if statement to check if array empty (array.length == 0) return 0, else do the math
+// use a for loop to add all elements in array
+// need a sum variable to store it after each loop
+// after done looping, divide by amount of numbers in array
+
+function findAverage(array) {
+  let sum = 0
+  if (array.length == 0) {
+    return 0
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+  }
+  const average = sum / array.length
+  return average
+}
